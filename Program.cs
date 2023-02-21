957dd43a9dc5a3bbeef6c1437eed336f07e281e7
P1VanDos <123485933+P1VanDos@users.noreply.github.com>
2023-02-22 00:26:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            /*int[] array = new int[12];
+            Random rand = new Random();
+            
+              for(int i=0 ; i<array.Length; i++) 
+                        {
+                            array[i] = rand.Next(0,111);
+                            if (array[i] < 100)
+                            {
+                                Console.WriteLine(array[i]);
+                            }
+
+                        }//---- 2(б)*/
+            /*     int[] array = {3,4,1,4,2,7,8,3,12} ;
+              for (int i = 0; i < array.Length; i++) 
+
+               {
+
+                   if (array[i] % 2 ==0)
+                   {
+                       Console.Write(array[i] + " ");
+                   }
+               }*///---- 2(в)
+
+
+
+
+
+            /*  int[] array = { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+              int negCount = 0;
+              foreach (int number in array)
+                  if (number < 0)
+                      negCount++;
+              Console.Write(negCount);*///---- 3
+
+            /* int[] array = new int[12];
+             Random rand = new Random();
+             for (int i=0;i< array.Length; i++) 
+             {
+                 array[i] = rand.Next(0,100);
+                 Console.WriteLine(array[i]*5);
+
+             }*///---- 5
+
+
+
+        }
+    }
+}
